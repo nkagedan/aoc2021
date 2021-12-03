8db984bddd3cf1df9cc3346cddb21081becdee0e
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-03 01:36:50 -0500

    solved 3b

--- day3.py
+++ day3.py
@@ -51,16 +51,55 @@ while len(df) > 1:
 
     df['char_x'] = df['raw_input'].str[character]
     counts = df['char_x'].value_counts()
-    if int(counts.loc[1]) >= int(counts.loc[0]):
+    counts
+    if int(counts.loc['1']) >= int(counts.loc['0']):
         target = '1'
     else:
         target = '0'
     df = df.loc[df['char_x'] == target]
 
+    len(df)
+
     character += 1
     if character > len(input_list[0]):
         character = 0
+    character
 
-df
+oxygen_binary = df['raw_input'].values[0]
+oxygen_binary
+oxygen_decimal = int(oxygen_binary,2)
+oxygen_decimal
 
-df['char_x'].value_counts().sort_values(ascending=False).index.values[0]
+# co2
+
+df = pd.DataFrame(input_list, columns =['raw_input'])
+
+character = 0
+
+while len(df) > 1:
+
+    df['char_x'] = df['raw_input'].str[character]
+    counts = df['char_x'].value_counts()
+    counts
+    if int(counts.loc['0']) <= int(counts.loc['1']):
+        target = '0'
+    else:
+        target = '1'
+    df = df.loc[df['char_x'] == target]
+
+    len(df)
+
+    character += 1
+    if character > len(input_list[0]):
+        character = 0
+    character
+
+co2_binary = df['raw_input'].values[0]
+co2_binary
+co2_decimal = int(co2_binary,2)
+co2_decimal
+
+# result
+
+life_support = oxygen_decimal*co2_decimal
+life_support
