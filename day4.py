5e24bcfba32257a65423f55c1dbe29af077b3f37
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-05 01:54:23 -0500

    solution for day 4 part 1

--- day4.py
+++ day4.py
@@ -30,28 +30,20 @@ for i in list(range(len(df)))[::6]:
 
 # Call numbers and find winning board
 
-bingo_boards[0]
-
-bingo_boards[0].iterrows()
-
-
-for i in range(len(call_list)):
-    current_call = call_list[:i+1]
-    for board in bingo_boards:
-        for x in range(0,5):
-            if all(item in current_call for item in board.iloc[x].tolist() or ):
-                winner = board
-                break
-            if all(item in current_call for item in board[x].tolist()):
-                winner = board
-                break
-
-i
-winner
+def check_for_winners(numbers, boards):
+    for i in range(len(numbers)):
+        current_call = numbers[:i+1]
+        for board in boards:
+            for x in range(0,5):
+                if (all(item in current_call for item in board.iloc[x].tolist())) or (all(item in current_call for item in board[x].tolist())):
+                    return (board, current_call)
 
 
+win_board, win_call = check_for_winners(call_list, bingo_boards)
 
-
+win_num = int(win_call[-1])
+unmarked = [int(i) for i in win_board.stack().tolist() if i not in win_call]
+sum(unmarked) * win_num
 
 
 '''Part 2'''
