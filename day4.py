29d54775e36f1814bb4ea9443fb06a0d7ecaefaa
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-05 01:30:56 -0500

    further work on day 4

--- day4.py
+++ day4.py
@@ -12,10 +12,14 @@ df = pd.DataFrame(input_list, columns =['raw_input'])
 
 '''Part 1'''
 
-bingo_list = df['raw_input'][0].split(',')
+# Get numbers to be called
+
+call_list = df['raw_input'][0].split(',')
 
 df = df[2:].reset_index()
-df.head(7)
+
+# Generate bingo boards
+
 bingo_boards = []
 
 counter = 0
@@ -24,26 +28,30 @@ for i in list(range(len(df)))[::6]:
     bingo_boards.append(pd.DataFrame(lists))
     counter += 1
 
-bingo_boards
+# Call numbers and find winning board
+
+bingo_boards[0]
+
+bingo_boards[0].iterrows()
 
 
-# Generate bingo boards
+for i in range(len(call_list)):
+    current_call = call_list[:i+1]
+    for board in bingo_boards:
+        for x in range(0,5):
+            if all(item in current_call for item in board.iloc[x].tolist() or ):
+                winner = board
+                break
+            if all(item in current_call for item in board[x].tolist()):
+                winner = board
+                break
 
-pd.DataFrame(df['raw_input'][2].split())
-
-l1 = df.iloc[2]['raw_input'].strip().split()
-l2 = df.iloc[3]['raw_input'].split()
-l3 = df.iloc[4]['raw_input'].split()
-l4 = df.iloc[5]['raw_input'].split()
-l5 = df.iloc[6]['raw_input'].split()
+i
+winner
 
 
 
-new_df = pd.DataFrame([l1,l2,l3,l4,l5])
-new_df.iloc[0,1]
-new_df
 
-df.head(20)
 
 
 '''Part 2'''
