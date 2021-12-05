7e1a8a32e4c32310364fece5305e959f17d1f676
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-05 00:37:12 -0500

    further work on day 4

--- day4.py
+++ day4.py
@@ -8,17 +8,24 @@ with open(file, 'r') as file:
     data = file.read().strip()
 
 input_list = data.splitlines()
-input_list
 df = pd.DataFrame(input_list, columns =['raw_input'])
-
 
 '''Part 1'''
 
-df.head(20)
-
 bingo_list = df['raw_input'][0].split(',')
 
-df.shape
+df = df[2:].reset_index()
+df.head(7)
+bingo_boards = []
+
+counter = 0
+for i in list(range(len(df)))[::6]:
+    lists = [df.iloc[x]['raw_input'].split() for x in range(i,i+5)]
+    bingo_boards.append(pd.DataFrame(lists))
+    counter += 1
+
+bingo_boards
+
 
 # Generate bingo boards
 
@@ -29,6 +36,8 @@ l2 = df.iloc[3]['raw_input'].split()
 l3 = df.iloc[4]['raw_input'].split()
 l4 = df.iloc[5]['raw_input'].split()
 l5 = df.iloc[6]['raw_input'].split()
+
+
 
 new_df = pd.DataFrame([l1,l2,l3,l4,l5])
 new_df.iloc[0,1]
