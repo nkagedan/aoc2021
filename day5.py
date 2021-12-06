59c029bd6a44e5670e99adc3b0aeaf4662127286
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-06 09:57:08 -0500

    5a progress

--- day5.py
+++ day5.py
@@ -1,6 +1,7 @@
 '''Load Input Data'''
 
 import pandas as pd
+import numpy as np
 
 file = 'input5test.txt'
 
@@ -12,7 +13,6 @@ df = pd.DataFrame(input_list, columns =['raw_input'])
 
 '''Part 1'''
 
-df['split'] = df['raw_input'].str.split(pat=',')
 df['x1']=df['raw_input'].str.split(pat=',').str[0].astype(int)
 df['y1']=df['raw_input'].str.split(pat=',').str[1].str.split(pat=' -> ').str[0].astype(int)
 df['x2']=df['raw_input'].str.split(pat=',').str[1].str.split(pat=' -> ').str[1].astype(int)
@@ -21,6 +21,15 @@ df['y2']=df['raw_input'].str.split(pat=',').str[2].astype(int)
 
 df = df.loc[(df['x1'] == df['x2']) | (df['y1'] == df['y2'])]
 df
+
+df[['x1','x2']].values.max()
+
+grid = pd.DataFrame(np.zeros((df[['x1','x2']].values.max(),df[['y1','y2']].values.max())), dtype=int)
+
+grid
+
+for i in range(len(df)):
+    
 
 
 '''Part 2'''
