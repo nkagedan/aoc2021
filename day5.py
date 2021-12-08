b57e622b01bdea5d788bf19f41ae21827c7246ed
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-08 01:22:15 -0500

    solved 5a

--- day5.py
+++ day5.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 
-file = 'input5test.txt'
+file = 'input5.txt'
 
 with open(file, 'r') as file:
     data = file.read().strip()
@@ -13,31 +13,40 @@ df = pd.DataFrame(input_list, columns =['raw_input'])
 
 '''Part 1'''
 
+def plot_line(row):
+    if row['x1'] == row['x2']:
+        grid.iloc[min(row[['y1', 'y2']]):max(row[['y1','y2']]+1),row['x1']] +=1
+    elif row['y1'] == row['y2']:
+        grid.iloc[row['y1'],min(row[['x1','x2']]):max(row[['x1','x2']])+1] +=1
+    else:
+        range(min(row.x1, row.x2),max(row.x1, row.x2))
+
+
+
 df['x1']=df['raw_input'].str.split(pat=',').str[0].astype(int)
 df['y1']=df['raw_input'].str.split(pat=',').str[1].str.split(pat=' -> ').str[0].astype(int)
 df['x2']=df['raw_input'].str.split(pat=',').str[1].str.split(pat=' -> ').str[1].astype(int)
 df['y2']=df['raw_input'].str.split(pat=',').str[2].astype(int)
 
+df['y2'].values.max()
+df.shape
 
-df = df.loc[(df['x1'] == df['x2']) | (df['y1'] == df['y2'])]
-df
+grid = pd.DataFrame(np.zeros((df[['x1','x2']].values.max()+5,df[['y1','y2']].values.max()+5)), dtype=int)
 
-df[['x1','x2']].values.max()
-
-grid = pd.DataFrame(np.zeros((df[['x1','x2']].values.max()+1,df[['y1','y2']].values.max()+1)), dtype=int)
-
-grid
-
-def plot_line(row):
-    if row['x1'] == row['x2']:
-        
-    elif row['y1'] == row['y2']:
-    else:
-        pass
 
 for i in range(len(df)):
-    print(f'{i=}')
-    print(type(df.iloc[i]))
+    pass
+    plot_line(df.iloc[i])
+
+grid
+result = 0
+
+for index, row in grid.iterrows():
+    for i in row.to_list():
+        if i >= 2:
+            result += 1
+
+print(result)
 
 
 '''Part 2'''
