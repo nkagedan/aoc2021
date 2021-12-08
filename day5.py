247eb9dcec819ba3bebcc3bbf86b1f4b58082b65
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-07 20:46:20 -0500

    5a progress

--- day5.py
+++ day5.py
@@ -24,12 +24,20 @@ df
 
 df[['x1','x2']].values.max()
 
-grid = pd.DataFrame(np.zeros((df[['x1','x2']].values.max(),df[['y1','y2']].values.max())), dtype=int)
+grid = pd.DataFrame(np.zeros((df[['x1','x2']].values.max()+1,df[['y1','y2']].values.max()+1)), dtype=int)
 
 grid
 
+def plot_line(row):
+    if row['x1'] == row['x2']:
+        
+    elif row['y1'] == row['y2']:
+    else:
+        pass
+
 for i in range(len(df)):
-    
+    print(f'{i=}')
+    print(type(df.iloc[i]))
 
 
 '''Part 2'''
