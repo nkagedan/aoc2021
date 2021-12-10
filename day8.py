458fe7fa7c20543255ff12262c1bf2819fbd9b81
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-10 00:13:02 -0500

    progress on 8b

--- day8.py
+++ day8.py
@@ -3,8 +3,14 @@
 
 import pandas as pd
 import numpy as np
+from itertools import groupby
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
-file = 'input8.txt'
+
+
+
+file = 'input8test.txt'
 
 with open(file, 'r') as file:
     data = file.read().strip()
@@ -12,7 +18,6 @@ with open(file, 'r') as file:
 input_list = data.splitlines()
 #input_list = [int(i) for i in data.split(',')]
 #df = pd.DataFrame(input_list, columns =['raw_input'])
-input_list
 #<codecell>
 
 '''Part 1'''
@@ -33,6 +38,45 @@ print(count)
 #<codecell>
 
 '''Part 2'''
+
+def find_output_value(entry):
+    print(entry)
+
+    digit_key = {}
+    decoder_digits = entry.split('|')[0].strip().split()
+    digit_key[1] = [list(sorted(i)) for i in decoder_digits if len(i) == 2][0]
+    digit_key[4] = [list(sorted(i)) for i in decoder_digits if len(i) == 4][0]
+    digit_key[7] = [list(sorted(i)) for i in decoder_digits if len(i) == 3][0]
+    digit_key[8] = [list(sorted(i)) for i in decoder_digits if len(i) == 7][0]
+    digit_key[3] = [list(sorted(i)) for i in decoder_digits if len(i) == 5 and all(elem in list(i) for elem in digit_key[7])][0]
+    digit_key[9] = list(sorted(''.join(set(digit_key[4]+digit_key[7]+digit_key[3]))))
+    digit_key[6] = [list(sorted(i)) for i in decoder_digits if len(i) == 6 and list(sorted(i)) not in digit_key.values()][0]
+    digit_key[5] = [list(sorted(i)) for i in decoder_digits if len(i) == 5 and all(elem in digit_key[6] for elem in list(i))][0]
+    digit_key[2] = [list(sorted(i)) for i in decoder_digits if len(i) == 5 and list(sorted(i)) not in digit_key.values()][0]
+
+
+
+    #pp.pprint(digit_key)
+
+    output_digits = entry.split('|')[1].strip().split()
+    output_digits
+    output_numbers = []
+    for i in output_digits:
+        for key in range(1,10):
+            if digit_key[key] == list(sorted(i)):
+                output_numbers.append(key)
+
+    result = int(''.join([str(i) for i in output_numbers]))
+
+    return result
+
+final_result = sum([find_output_value(i) for i in input_list])
+
+print(f'{final_result=}')
+
+
+
+
 
 
 
