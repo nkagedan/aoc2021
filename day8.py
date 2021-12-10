db17b7ed825bb8b811df8d1932fb923cc3ba4a83
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-10 09:51:21 -0500

    solved 8b

--- day8.py
+++ day8.py
@@ -1,14 +1,10 @@
 #<codecell>
-'''Load Input Data'''
+'''Setup'''
 
 import pandas as pd
 import numpy as np
-from itertools import groupby
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
-
-
-
 
 file = 'input8.txt'
 
@@ -18,8 +14,9 @@ with open(file, 'r') as file:
 input_list = data.splitlines()
 #input_list = [int(i) for i in data.split(',')]
 #df = pd.DataFrame(input_list, columns =['raw_input'])
+
 #<codecell>
-len(input_list)
+
 '''Part 1'''
 
 unique_char_counts = [2, 4, 3, 7]
@@ -34,7 +31,6 @@ for i in input_list:
 
 print(f'part 1 result = {count}')
 
-
 #<codecell>
 
 '''Part 2'''
@@ -48,57 +44,31 @@ def find_output_value(entry):
     digit_key[7] = [list(sorted(i)) for i in decoder_digits if len(i) == 3][0]
     digit_key[8] = [list(sorted(i)) for i in decoder_digits if len(i) == 7][0]
     digit_key[3] = [list(sorted(i)) for i in decoder_digits if len(i) == 5 and all(elem in list(i) for elem in digit_key[7])][0]
-    if len([list(sorted(i)) for i in decoder_digits if len(i) == 5 and all(elem in list(i) for elem in digit_key[7])])>1:
-        print(f'problem for 3 at {entry}')
     digit_key[9] = list(sorted(''.join(set(digit_key[4]+digit_key[3]))))
-
     digit_key[0] = [list(sorted(i)) for i in decoder_digits if (len(i) == 6) and (all(elem in list(i) for elem in digit_key[1])) and (list(sorted(i)) not in digit_key.values())][0]
-    if len([list(sorted(i)) for i in decoder_digits if (len(i) == 6) and (all(elem in list(i) for elem in digit_key[1])) and (list(sorted(i)) not in digit_key.values())])>1:
-        print(f'problem for 0 at {entry}')
-
     digit_key[6] = [list(sorted(i)) for i in decoder_digits if len(i) == 6 and list(sorted(i)) not in digit_key.values()][0]
-    if len([list(sorted(i)) for i in decoder_digits if len(i) == 6 and list(sorted(i)) not in digit_key.values()])>1:
-        print(f'problem for 6 at {entry}')
-    #solve 5
-    '''candidates = [list(sorted(i)) for i in decoder_digits if len(i) == 5]
-    for i in candidates:
-        if all([elem in digit_key[6] for elem in list(i)]):
-            digit_key[5] = sorted(i)'''
-
-
     digit_key[5] = [list(sorted(i)) for i in decoder_digits if len(i) == 5 and all(elem in digit_key[6] for elem in list(i))][0]
-    if len([list(sorted(i)) for i in decoder_digits if len(i) == 5 and all(elem in digit_key[6] for elem in list(i))])>1:
-        print(f'problem for 5 at {entry}')
     digit_key[2] = [list(sorted(i)) for i in decoder_digits if len(i) == 5 and list(sorted(i)) not in digit_key.values()][0]
-    if len([list(sorted(i)) for i in decoder_digits if len(i) == 5 and list(sorted(i)) not in digit_key.values()])>1:
-        print(f'problem for 2 at {entry}')
 
-    #pp.pprint(digit_key)
     output_digits = entry.split('|')[1].strip().split()
-    output_digits
+
     output_numbers = []
     for i in output_digits:
-        for key in range(1,10):
+        for key in range(0,10):
             if digit_key[key] == list(sorted(i)):
                 output_numbers.append(key)
 
     result = int(''.join([str(i) for i in output_numbers]))
+
+    pp.pprint(digit_key)
+    print(output_digits)
+    print(output_numbers)
+    print(result)
 
     return result
 
 final_result = sum([find_output_value(i) for i in input_list])
 
 print(f'part 2 result = {final_result}')
-
-
-'''
-guesses:
-698457 too low
-'''
-
-
-
-
-
 
 #<codecell>
