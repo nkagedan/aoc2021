9bac1fe8428471527edd47b211c76dbd3516e420
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-11 22:41:56 -0500

    started 9b

--- day9.py
+++ day9.py
@@ -18,8 +18,8 @@ df = pd.DataFrame(input_list, columns =['raw_input'])
 #<codecell>
 
 '''Part 1'''
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_columns', 10)
 
 
 df = pd.DataFrame(np.zeros([len(input_list),len(input_list[0])], dtype=int))
@@ -47,6 +47,20 @@ print(f'{df.shape=}, {cells_checked=}')
 #<codecell>
 
 '''Part 2'''
+
+df
+
+visited = []
+basin_size = 0
+
+def find_basin_size(coordinates,visited,df):
+    if coordinates not in visited and df.iat[coordinates] != 9:
+        basin_size += 1
+        visited.append(coordinates)
+        search_area = df.iloc[max(0,(y-1)):(y+2),max(0,(x-1)):(x+2)]
+        for neighbor in search_area:
+            find_basin_size(neighbor)
+
 
 
 
