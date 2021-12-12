92560e90b1dce88b5c0089d73c413717753d00e2
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-11 21:08:03 -0500

    solved 9a

--- day9.py
+++ day9.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
-
 file = 'input9.txt'
 
 with open(file, 'r') as file:
@@ -15,15 +14,16 @@ input_list = data.splitlines()
 #input_list = [int(i) for i in data.split(',')]
 df = pd.DataFrame(input_list, columns =['raw_input'])
 
+
 #<codecell>
 
 '''Part 1'''
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
-input_list
-df
-len(input_list[0])
 
 df = pd.DataFrame(np.zeros([len(input_list),len(input_list[0])], dtype=int))
+
 
 for i in range(len(input_list)):
     df.loc[i] = [int(x) for x in list(input_list[i])]
@@ -37,18 +37,13 @@ for y in range(len(df)):
         #print(f'{y=} {x=}')
         target = int(df.iat[y,x])
         search_area = df.iloc[max(0,(y-1)):(y+2),max(0,(x-1)):(x+2)].stack().tolist()
-        if target == min(search_area):
+        if target == min(search_area) and target != 9:
             print(f'adding {target+1} at {y=} {x=}')
             risk_level += (target+1)
 
-print(risk_level)
+print(f'{risk_level=}')
 print(f'{df.shape=}, {cells_checked=}')
 
-df.iloc[max(0,(0-1)):(0+2),max(0,(0-1)):(0+2)]
-
-df.iloc[0:2,0:2]
-
-df
 #<codecell>
 
 '''Part 2'''
