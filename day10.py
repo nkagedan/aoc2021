fad551666dab89cb439c7e2870ee5e6c2a53b730
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-13 23:07:55 -0500

    solved 10a

--- day10.py
+++ day10.py
@@ -5,8 +5,9 @@ import pandas as pd
 import numpy as np
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
+import json
 
-file = 'input10test.txt'
+file = 'input10.txt'
 
 with open(file, 'r') as file:
     data = file.read().strip()
@@ -19,19 +20,43 @@ input_list = data.splitlines()
 
 '''Part 1'''
 
-input_list
+char_sets = {
+    ')':'(',
+    ']':'[',
+    '}':'{',
+    '>':'<'
+}
 
-character_sets = {'(':')','[':']','{':'}','<':'>'}
-df=pd.DataFrame(character_sets)
 
+char_vals = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
 
-pp.pprint(character_sets)
+print(json.dumps(char_sets, indent=4))
+
+def find_errors(line):
+    opened_stack = []
+    for char in line:
+        if char in char_sets.values():
+            opened_stack.append(char)
+        else:
+            if opened_stack[-1] != char_sets[char]:
+                error_chars.append(char)
+                return None
+            else:
+                opened_stack.pop()
+
+error_chars = []
 
 for i in input_list:
-    print(sorted(i))
-    for char in sorted(i):
-        pass#print(char)
-    print('\n\n\n')
+    find_errors(i)
+
+result = sum([char_vals[i] for i in error_chars])
+
+print(result)
 
 #<codecell>
 
