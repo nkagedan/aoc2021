319199defc9559160c7672a634ef603b445efe68
nkagedan <87455738+nkagedan@users.noreply.github.com>
2021-12-15 00:46:04 -0500

    starting day 11

---
+++ day11.py
@@ -0,0 +1,28 @@
+#<codecell>
+'''Setup'''
+
+import pandas as pd
+import numpy as np
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+import json
+
+file = 'input10.txt'
+
+with open(file, 'r') as file:
+    data = file.read().strip()
+
+input_list = data.splitlines()
+#input_list = [int(i) for i in data.split(',')]
+#df = pd.DataFrame(input_list, columns =['raw_input'])
+
+#<codecell>
+
+'''Part 1'''
+
+
+#<codecell>
+
+'''Part 2'''
+
+#<codecell>
